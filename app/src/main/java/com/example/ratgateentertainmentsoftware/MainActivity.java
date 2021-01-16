713f8edd02d5164995e3bc20e75669270b252863
Ratgate <rm.rafal.markiewicz@gmail.com>
2021-01-16 14:21:28 +0100

    There is use for heads and tails now. Both are Ids for images.
     Clicked() is bugged and occurs crashes.
     First clicked image works. second does't.
    
    Player should be not allowed to be able to click squares until method play() is fired, so either play should be fired in onCreate() or gridLayout will be invisible, until player clicks the Shuffle button.
    
    There are unused code, that could use a clean up.

--- app/src/main/java/com/example/ratgateentertainmentsoftware/MainActivity.java
+++ app/src/main/java/com/example/ratgateentertainmentsoftware/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -16,21 +17,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    String[] heads = {
-            "R.drawable.pic01", "R.drawable.pic02", "R.drawable.pic03", "R.drawable.pic04", "R.drawable.pic05", "R.drawable.pic06", 
-            "R.drawable.pic07", "R.drawable.pic08", "R.drawable.pic09", "R.drawable.pic10", "R.drawable.pic11", "R.drawable.pic12", 
-            "R.drawable.pic13", "R.drawable.pic14", "R.drawable.pic15", "R.drawable.pic16", "R.drawable.pic17", "R.drawable.pic18", 
-            "R.drawable.pic19", "R.drawable.pic20", "R.drawable.pic21", "R.drawable.pic22", "R.drawable.pic23", "R.drawable.pic24"};
-    String tail = "R.drawable.pic00";
-    ArrayList<ImageView> imageIds = new ArrayList<ImageView>();
-  //  GridLayout gridLayout = findViewById(R.id.gridLayout);
+    int[] heads = {
+            R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5, R.drawable.pic6,
+            R.drawable.pic7, R.drawable.pic8, R.drawable.pic9, R.drawable.pic10, R.drawable.pic11, R.drawable.pic12};
+    int tail = R.drawable.pic00;
+    ArrayList<ImageView> imageViews = new ArrayList<>();
+//    android.widget.GridLayout gridLayout;
+//    GridLayout gridLayout;
+//    androidx.gridlayout.widget.GridLayout gridLayout = findViewById(R.id.gridLayout);
     Integer[] cardsByOrder = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12};
+
 
     Boolean victorious;
     List<Integer> assignedCard;
     int[] gameState = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     Boolean toggledCard;
     ImageView previous;
+//    Button button;
 
 
     @Override
@@ -38,45 +41,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        previous = findViewById(R.id.imageView1);
 
-        imageIds.add(findViewById(R.id.imageView1));
-        imageIds.add(findViewById(R.id.imageView2));
-        imageIds.add(findViewById(R.id.imageView3));
-        imageIds.add(findViewById(R.id.imageView4));
-        imageIds.add(findViewById(R.id.imageView5));
-        imageIds.add(findViewById(R.id.imageView6));
-        imageIds.add(findViewById(R.id.imageView7));
-        imageIds.add(findViewById(R.id.imageView8));
-        imageIds.add(findViewById(R.id.imageView9));
-        imageIds.add(findViewById(R.id.imageView10));
-        imageIds.add(findViewById(R.id.imageView11));
-        imageIds.add(findViewById(R.id.imageView12));
-        imageIds.add(findViewById(R.id.imageView13));
-        imageIds.add(findViewById(R.id.imageView14));
-        imageIds.add(findViewById(R.id.imageView15));
-        imageIds.add(findViewById(R.id.imageView16));
-        imageIds.add(findViewById(R.id.imageView17));
-        imageIds.add(findViewById(R.id.imageView18));
-        imageIds.add(findViewById(R.id.imageView19));
-        imageIds.add(findViewById(R.id.imageView20));
-        imageIds.add(findViewById(R.id.imageView21));
-        imageIds.add(findViewById(R.id.imageView22));
-        imageIds.add(findViewById(R.id.imageView23));
-        imageIds.add(findViewById(R.id.imageView24));
 
-   //     play();
+
+        imageViews.add(findViewById(R.id.imageView1));
+        imageViews.add(findViewById(R.id.imageView2));
+        imageViews.add(findViewById(R.id.imageView3));
+        imageViews.add(findViewById(R.id.imageView4));
+        imageViews.add(findViewById(R.id.imageView5));
+        imageViews.add(findViewById(R.id.imageView6));
+        imageViews.add(findViewById(R.id.imageView7));
+        imageViews.add(findViewById(R.id.imageView8));
+        imageViews.add(findViewById(R.id.imageView9));
+        imageViews.add(findViewById(R.id.imageView10));
+        imageViews.add(findViewById(R.id.imageView11));
+        imageViews.add(findViewById(R.id.imageView12));
+        imageViews.add(findViewById(R.id.imageView13));
+        imageViews.add(findViewById(R.id.imageView14));
+        imageViews.add(findViewById(R.id.imageView15));
+        imageViews.add(findViewById(R.id.imageView16));
+        imageViews.add(findViewById(R.id.imageView17));
+        imageViews.add(findViewById(R.id.imageView18));
+        imageViews.add(findViewById(R.id.imageView19));
+        imageViews.add(findViewById(R.id.imageView20));
+        imageViews.add(findViewById(R.id.imageView21));
+        imageViews.add(findViewById(R.id.imageView22));
+        imageViews.add(findViewById(R.id.imageView23));
+        imageViews.add(findViewById(R.id.imageView24));
+
+//        gridLayout.setVisibility(View.INVISIBLE);
     }
 
-    public void play(View view){
- //       gridLayout.setVisibility(View.VISIBLE);
+    public void shuffle(View view){
+        Log.i("Postep", "kliknieto shuffle");
+//        gridLayout.setVisibility(View.VISIBLE);
         Log.i("Postep", "Play w ogule odpalil");
         victorious = false;
-        toggledCard = false;
         toggledCard = false;
         Log.i("Postep", "ustalil wartosc zmiennych");
         //Wyzeroj stan gry, odwroc wszestkie karty rewersami do gory, przypisz nowe awersy do kart
         assignedCard = Arrays.asList(cardsByOrder);
+        Log.i("Postep", assignedCard.toString());
         Collections.shuffle(assignedCard);
+        Log.i("Postep", assignedCard.toString());
         Log.i("Postep", "Pomieszal tablice kart");
 
         for (int state:gameState) {
@@ -85,33 +93,38 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Postep", "wyzerowal stan gry");
 
 
-        for (ImageView imageView:imageIds) {
-            imageView.setImageResource(R.drawable.pic00);
+        for (ImageView imageView: imageViews) {
+            imageView.setImageResource(tail);
         }
         Log.i("Postep", "Odwrocil karty rewersami do gory");
     }
 
-    public void clicked(View view){
+    public void clicked(View view) throws InterruptedException {
         ImageView clickedCard = (ImageView) view;
-        Log.i("Postep", "Kliknieto" + clickedCard.getTag().toString());
-
-        String tempString;
         int clickedTag = Integer.parseInt(clickedCard.getTag().toString());
+        int previousTag = Integer.parseInt(previous.getTag().toString());
+        Log.i("Postep", "Kliknieto " + clickedCard.getTag().toString());
+
+//        String tempString;
+
         //Sprawdz, czy kliklal w awers czy rewers
-        if(gameState[clickedTag]==0){
+        if(gameState[clickedTag-1]==0){
             //obroc karte, zmien stan gry,
 
-            tempString = "pic" + clickedTag;
-            int resID = getResources().getIdentifier(tempString , "drawable", getPackageName());
-            clickedCard.setImageResource(resID);
+//            tempString = "pic" + clickedTag;
+//            int resID = getResources().getIdentifier(tempString , "drawable", getPackageName());
+
+            clickedCard.setImageResource(heads[assignedCard.get(clickedTag-1)]);
             gameState[clickedTag-1] = 1;
 
             // sprawdz czy dobrze, zostaw jak tak, schowaj jak nie, kontynuuj jak nie ma do porownania
             //Sprawdz,  czy jest to pierwsze klikniecie
 
             if(toggledCard){
+                Thread.sleep(500);
                 //Sprawdz, czy para
-                if(assignedCard.get(Integer.parseInt(previous.getTag().toString()) - 1) == assignedCard.get(clickedTag)){
+                if(assignedCard.get(previousTag - 1).equals(assignedCard.get(clickedTag - 1))){
+                    toggledCard = false;
                     //Sprawdz, czy wygrałes
                     victorious = true;
                     for (int state:gameState) {
@@ -121,10 +134,10 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 } else {
-                    gameState[Integer.parseInt(previous.getTag().toString()) - 1] = 0;
-                    gameState[clickedTag-1] = 0;
-                    clickedCard.setImageResource(R.drawable.pic00);
-                    previous.setImageResource(R.drawable.pic00);
+                    gameState[previousTag - 1] = 0;
+                    gameState[clickedTag - 1] = 0;
+                    clickedCard.setImageResource(tail);
+                    previous.setImageResource(tail);
                     }
                 toggledCard = false;
             } else {
